9e4f91e0cce80378ef2503a6d090502411096776
Pr1ntel <vandushev.sergey@yandex.ru>
2023-04-26 01:44:48 +0300

    add FindFilm

--- src/components/optionalFilms/FindFilm.jsx
+++ src/components/optionalFilms/FindFilm.jsx
@@ -1,10 +1,85 @@
 import React from 'react';
+import {Table, Col, Row, Input, Space, Select, Button} from "antd";
+import MenuItem from "../menu/MenuItem";
 
 const FindFilm = () => {
+    const columns = [
+        {
+            title: 'Название',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Дата выхода',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'Длительность(Минут)',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: 'Фото',
+            dataIndex: 'photo',
+            key: 'photo',
+        },
+
+    ];
     return (
         <div>
-            <h1>Hello</h1>
+            <Row>
+                <Col span={4}><MenuItem/></Col>
+                <Col span={12}>
+                    <div style={{opacity: 0.9}}>
+                        <Table
+                            columns={columns}
+                            expandable={{
+                                expandedRowRender: (record) => (
+                                    <p
+                                        style={{
+                                            margin: 0,
+                                        }}
+                                    >
+                                        {record.description}
+                                    </p>
+                                ),
+                                rowExpandable: (record) => record.name !== 'Not Expandable',
+                            }}
+
+
+                        />
+                    </div>
+                </Col>
+                <Col span={8}>
+                    <h2 style={{
+                        color:"blue",
+                    }}>Поиск фильма</h2>
+                    <div>
+                        <Input placeholder="Название фильма" style={{
+                            width: '40%'
+                        }}/>
+                    </div>
+                    <div>
+                        <Space
+                            direction="vertical"
+                            style={{
+                                width: '40%',
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <Button type="primary"
+                                style={{
+                                    width: '13%',
+                                }}>
+                            Поиск
+                        </Button>
+                    </div>
+                </Col>
+            </Row>
         </div>
+
     );
 };
 
